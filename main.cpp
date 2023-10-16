25c91ecfb6e02183eab2c03656dba0fa66acbddd
FeiFei0827 <2035585231@qq.com>
2023-10-16 11:39:16 +0800

    还有问题 需要更改

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by 飞飞 on 2023/10/12.
+//
+//#include "cmake-build-debug/Vector.h"
+
+#include "iostream"
+#include "Vector.h"
+int main()
+{
+//int a[]={1,2,3};
+Vector<int> b(4,5,6);
+b.search(4);
+}
